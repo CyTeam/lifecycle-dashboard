aed8fdf84bb37eb60f9a054e9b02d72bfaca433d
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-05-14 10:06:52 +0200

    Initialize Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifecycleDashboard::Application.config.secret_token = '0f4962e6848d9dfa5628b04888532fe9d55ca350166f7097868af6a764a4544efbf3585026f0dec8f6a04f93fc9d8de549da75d6c0c945833d2bc9332f0fadaa'
